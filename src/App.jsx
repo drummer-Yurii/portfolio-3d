f8ba97f7980f2c0b92e175a73a7fc3c4a0d9abe4
Yurii Rybachok <drummer2009@ukr.net>
2023-03-10 12:20:04 +0200

    add logo to navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,14 @@ const App = () => {
           <Hero />
         </div>
         <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   )
