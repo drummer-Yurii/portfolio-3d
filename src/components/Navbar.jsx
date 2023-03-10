1e4b596922b9cdedf45798e60d3a89b6b27fa098
Yurii Rybachok <drummer2009@ukr.net>
2023-03-10 12:35:14 +0200

    show pages in navbar & navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,18 @@ const Navbar = () => {
               Yurii <span className='sm:block hidden'>| JavaScript / React</span>
             </p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title ? 'text-white' : 'text-secondary'} 
+              hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
