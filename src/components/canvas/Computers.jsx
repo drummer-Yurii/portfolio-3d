2e8cc7609cbc65f8b521efb4ced8ef2d72e1e852
Yurii Rybachok <drummer2009@ukr.net>
2023-03-11 09:56:59 +0200

    add title & description to about component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,14 @@
-import { React, Suspense, useEffect, useState} from 'react';
+import React, { Suspense, useEffect, useState} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Computers = ({isMobile}) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
       <spotLight 
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,6 +17,7 @@ const Computers = ({isMobile}) => {
         castShadow
         shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive 
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -44,14 +44,15 @@ const ComputersCanvas = () => {
 
    return () => {
     mediaQuery.removeEventListener('change', handleMediaQueryChange);
-   }
-  }, [])
+   };
+  }, []);
   
 
   return (
     <Canvas
       frameloop="demand"
       shadows
+      dpr={[1, 2]}
       camera={{position: [20, 3, 5], fov: 25}}
       gl={{preserveDrawingBuffer: true}}
     >
@@ -65,7 +66,7 @@ const ComputersCanvas = () => {
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default ComputersCanvas;
